d7de1434982ba15005891e5d9415363d35a9b416
ZAM <p602985@yeah.net>
2022-11-23 17:58:07 +0800

    测试config函数

--- experiment/cli/electronup.config.ts
+++ experiment/cli/electronup.config.ts
@@ -1,3 +1,28 @@
-import { ElectronupConfig } from '@quiteer/electronup'
+import { resolve } from 'path'
+import { buildConfig, defineConfig, tsupConfig, viteConfig } from '@quiteer/electronup'
+import type { ConfigEnv } from '@quiteer/electronup'
 
-export default '这是根目录得配置，也是默认配置'
+const viteOptions = viteConfig((env: ConfigEnv) => {
+  console.log('env: ', env)
+  return {
+    root: resolve('./src', 'render')
+  }
+})
+
+const tsupOptions = tsupConfig((env: ConfigEnv) => {
+  console.log('env: ', env)
+  return {
+  }
+})
+
+const builderOptions = buildConfig((env: ConfigEnv) => {
+  console.log('env: ', env)
+  return {
+  }
+})
+
+export default defineConfig({
+  viteConfig: viteOptions,
+  tsupConfig: tsupOptions,
+  builderConfig: builderOptions
+})
