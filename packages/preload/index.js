8e3291967323d1ae090b635e7e2ac8a8af4a673b
ZAM <p602985@yeah.net>
2022-12-16 17:40:36 +0800

    修改函数命名

--- packages/preload/index.js
+++ packages/preload/index.js
@@ -2,9 +2,9 @@ const path = require('path')
 
 const pathFile = path.join(__dirname, 'dist', 'preload.js')
 
-function getElectronPath() {
+function getPreloadPath() {
   return pathFile
 }
 
-module.exports = getElectronPath()
+module.exports = getPreloadPath()
 
