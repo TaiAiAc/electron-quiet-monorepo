96ccc5d5b16212cae5e7d61ba8abfdc376c99130
quiteer <p602985@yeah.net>
2023-04-22 14:41:59 +0800

    bug | 修复类型不匹配的bug

--- packages/electronup/src/transform/getExportConfig.ts
+++ packages/electronup/src/transform/getExportConfig.ts
@@ -2,15 +2,20 @@ import type { ElectronupConfig, ElectronupConfigFn, UserElectronupConfig } from 
 import { getElectronupConfig } from '../default/electronup.config'
 import { store } from '../utils'
 
+/**
+ * Description  解析用户自定义配置···
+ * @param {any} config:UserElectronupConfig
+ * @returns {any}
+ */
 const exportElectronupConfig = (config: UserElectronupConfig): ElectronupConfig => {
   const typeStr = typeof config
   if (typeStr === 'function') {
-    const option = (<ElectronupConfigFn>config)({ command: store.command, root: store.root })
+    const option = (config as ElectronupConfigFn)({ command: store.command, root: store.root })
     return option
   }
 
   if (typeStr === 'object')
-    return <ElectronupConfig>config
+    return config as ElectronupConfig
 
   throw new Error('electronup 配置错误,解析失败！')
 }
